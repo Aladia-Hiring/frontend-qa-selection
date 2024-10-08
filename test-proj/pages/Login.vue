50ad4a9f704c55b66567232fcf2976670f811db5
c7-2know <betelhemyimamd@gmail.com>
2024-10-08 14:49:44 +0300

    set up test project

---
+++ test-proj/pages/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <img src="../assets/logo.jpg" alt="Nuxt.js logo" />
+        <h1>Aladian </h1>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'Login'
+};
+</script>
