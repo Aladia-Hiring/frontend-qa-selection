4ed9033b6d591cf185c54803cca02c8d7c3d5470
Dr. Frankenstein <89076459+firaeholic@users.noreply.github.com>
2024-10-08 14:57:08 +0300

    login page

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <main>
+      <Login />
+    </main>
+  </div>
+</template>
+
+<script setup>
+// Import the Login component
+import Login from '~/components/Login.vue'
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+  margin-bottom: 20px;
+}
+</style>
