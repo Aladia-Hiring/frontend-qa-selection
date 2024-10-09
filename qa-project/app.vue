e3883bf6b2a0a1fdb44a86533afa28409672cb85
BirhanuAsmamaw <birhanuasmamaw24@gmail.com>
2024-10-09 10:14:35 +0300

    reconstruct the components

---
+++ qa-project/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex justify-center bg-black w-full h-full p-3">
+    <index />
+  </div>
+</template>
+
+<script>
+import index from "./pages/login/index.vue";
+export default {
+  components: { index },
+};
+</script>
