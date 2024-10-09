c5975f28bc6280210263012f892b42639725ee4b
Yeshiwas Tikimu <yeshiwastikimu0@gmail.com>
2024-10-09 11:38:06 +0300

    cypress test added

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -2,6 +2,8 @@ describe('Nuxt.js Home Page', () => {
     it('should load the home page', () => {
       cy.visit('/');
       cy.contains('Welcome to Nuxt!');
+      cy.contains('Welcome');
+
     });
   });
   
